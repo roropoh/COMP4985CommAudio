386ca5db2a6b0ae5dee1079b5495154f84e94869
neotimbo <neotimmy@hotmail.com>
2014-03-28 16:01:31 -0700

    Added IP box and Port number box

--- CommAudio/MyForm.cpp
+++ CommAudio/MyForm.cpp
@@ -1,2 +1,22 @@
 #include "MyForm.h"
 
+using namespace CommAudio;
+
+int WINAPI WinMain(HINSTANCE hInstance,
+	HINSTANCE hPrevInstance, LPSTR lpCmdLine, int nShowCmd)
+{
+	WORD wVersionRequested;
+	WSADATA WSAData;
+
+	wVersionRequested = MAKEWORD(2, 2);
+
+	//Call WSAStartup
+	if (WSAStartup(wVersionRequested, &WSAData) != 0){
+		MessageBox::Show("WSAStartup failed with error (" + WSAGetLastError() + ")", "Error");
+		WSACleanup();
+		exit(1);
+	}
+
+	Application::EnableVisualStyles();
+	Application::Run(gcnew MyForm());
+}
